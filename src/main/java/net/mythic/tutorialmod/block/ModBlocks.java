404f11b7333f9e7e4c3669329cd8f39212296149
Mythic-Modding <mythic.modding@gmx.de>
2025-09-01 21:34:25 +0200

    added ModBlocks

--- src/main/java/net/mythic/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/mythic/tutorialmod/block/ModBlocks.java
@@ -1,4 +1,10 @@
 package net.mythic.tutorialmod.block;
 
+import net.mythic.tutorialmod.TutorialMod;
+
 public class ModBlocks {
+
+    public static void registerModBlocks() {
+        TutorialMod.LOGGER.info("Registering Mod for " + TutorialMod.MOD_ID);
+    }
 }
