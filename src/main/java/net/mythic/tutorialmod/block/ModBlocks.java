ba1fd584721278cebe5c9b10c7d0fc7ef7b7d57d
Mythic-Modding <mythic.modding@gmx.de>
2025-09-02 15:32:22 +0200

    added raw pink garnet

--- src/main/java/net/mythic/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/mythic/tutorialmod/block/ModBlocks.java
@@ -17,7 +17,9 @@ public class ModBlocks {
             new Block(AbstractBlock.Settings.create().strength(4f)
                     .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
 
-
+    public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
+            new Block(AbstractBlock.Settings.create().strength(3f)
+                    .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
