29412aa9cf00140a68b76268a04fdbe9521d2c1c
Mythic-Modding <mythic.modding@gmx.de>
2025-09-06 18:48:53 +0200

    idk what i changed

--- src/main/java/net/mythic/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/mythic/tutorialmod/block/ModBlocks.java
@@ -18,7 +18,6 @@ public class ModBlocks {
     public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
             new Block(AbstractBlock.Settings.create().strength(4f)
                     .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
-
     public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
             new Block(AbstractBlock.Settings.create().strength(3f)
                     .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
